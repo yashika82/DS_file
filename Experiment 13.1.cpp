a5282e9e5340bc514d5b201afd76a908954093f8
yashika82 <44964891+yashika82@users.noreply.github.com>
2018-11-16 09:29:30 +0530

    Update Experiment 13.1.cpp

--- Experiment 13.1.cpp
+++ Experiment 13.1.cpp
@@ -5,12 +5,12 @@ int a[10][10],i,j,k,n,qu[10],front,rare,v,visit[10],visited[10],x,y;
 
 int main()
 {
-cout <<"enter no of vertices SIR/MAM!";
+cout <<"enter no of vertices";
 cin >> n;
-cout<<"sir please enter for undirected grah"<<endl;
+cout<<"enter for undirected grah"<<endl;
      cin>>x>>y;
     if(x>=n&&y>=n)
-        cout<<"sir ji please enter within range"<<endl;
+        cout<<"please enter within range"<<endl;
         else
         {
     while(x!=-1&&y!=-1)
